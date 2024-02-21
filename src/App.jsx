0049cc5d15ec35e270fd18b7cbf7c7536646e1f2
diegodorado78 <diegodorado78@gmail.com>
2024-02-21 15:58:48 -0500

    new buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { useRef, useState } from 'react';
 import { Button } from "@nextui-org/react";
 import video from './assets/MrMascadedo.mp4'
 import annyang from 'annyang'
+import { FaMicrophoneAlt } from "react-icons/fa";
+
 const sharedStyles = 'border p-2 font-semibold'
 
 function App() {
   const videoRef = useRef(null);
   const [userText, setUserText] = useState('')
   const [speaking, setSpeaking] = useState(false)
+  const [listening, setListening] = useState(false)
 
   const anything = (anything) => {
     setUserText(anything)
@@ -42,17 +45,28 @@ function App() {
   const handleAsk = () => {
     voz.cancel()
     annyang.addCommands(commands);
-    annyang.start()
+
+    if (listening) {
+      annyang.abort()
+      setListening(false)
+
+    } else {
+      annyang.start()
+      setListening(true)
+
+
+    }
   }
 
   // Get answer
   const handleGetAnswer = () => {
     annyang.abort()
+    setListening(false)
     videoRef.current && videoRef.current.play()
     fetch("https://google.serper.dev/search", requestOptions)
       .then(response => response.json())
       .then(result => {
-        console.log(result.organic[0].snippet)
+        console.log(result)
         readText(result.organic[0].snippet)
         return setSpeaking(true)
       })
@@ -74,10 +88,22 @@ function App() {
   return (
     <div className='w-full sm:w-6/12 mx-auto text-bold'>
       <video muted ref={videoRef} className='rounded-xl m-1' src={video}></video>
-      <Button onClick={handleAsk} color="success" className={sharedStyles}>Ask a question</Button>
-      <Button onClick={handleGetAnswer} color="primary" className={sharedStyles}>Get answer</Button>
-      <Button onClick={handleStopAnswer} color="danger" className={sharedStyles}>Stop answering</Button>
-      <div className='texting'> you said: {userText}</div>
+      <Button onClick={handleAsk} color="success" className={` w-12 h-12  rounded-full`}><FaMicrophoneAlt className='h-10 w-10' /></Button>
+
+      <div className='flex flex-col w-full mx-auto'>
+        {listening && !userText &&
+          <div>
+            <p className=" w-8 h-8 border-t-4 border-green-600 rounded-full animate-spin mx-auto"></p>
+            <p className='text-green-600 text-lg'>Listening...</p>
+          </div>
+        }
+        {userText && <div>
+          <p className='text-default-500 text-lg my-4'> You said: {userText}  </p>
+          <Button onClick={handleGetAnswer} color="primary" className={sharedStyles}>Get answer</Button>
+          <Button onClick={handleStopAnswer} color="danger" className={sharedStyles}>Stop answering</Button>
+        </div>}
+      </div>
+
     </div>
   )
 }
